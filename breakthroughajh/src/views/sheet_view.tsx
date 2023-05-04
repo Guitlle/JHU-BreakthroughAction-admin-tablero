870021c37ba6be5c588c386fd8f0b53a283f31a8
Guillermo <lgar89@gmail.com>
2023-05-04 08:10:15 -0600

    remove hard coded

--- breakthroughajh/src/views/sheet_view.tsx
+++ breakthroughajh/src/views/sheet_view.tsx
@@ -1,3 +1,3 @@
 export default function ExampleCMSView() {
-  return <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQ56AaONWi-1QlYYeAd1wV85zZDZeSYdk_Q82GNXOIrXMLdI5rjuNXxjH4AYFikHRAYcOxGODFQYsQL/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false" style={{width: '100%', height: '100%'}}></iframe>
+
 }
